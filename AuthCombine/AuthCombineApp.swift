e564857298185dbe728df04cc54df5a571f68264
Emilia <yourbelovedemilia@yandex.ru>
2024-04-08 23:03:34 +0300

    Initial Commit

---
+++ AuthCombine/AuthCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthCombineApp.swift
+//  AuthCombine
+//
+//  Created by Эмилия on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
